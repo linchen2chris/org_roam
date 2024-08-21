3b6789ee2c7bdad0fc3cdee370aae76bda1d2b9e
linchen2chris <lc1990linux@gmail.com>
2024-08-21 17:58:32 +0800

    add search for notes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,8 +18,26 @@ class _MyHomePageState extends State<MyHomePage> {
   OrgDocument root = OrgDocument.parse('Loading notes');
 
   List<FileSystemEntity> notes = [];
+  List<FileSystemEntity> filteredNotes = [];
 
   int selectedNote = 0;
+
+  void search(String query) {
+    if (query.isEmpty) {
+      setState(() {
+        filteredNotes = notes;
+      });
+      return;
+    }
+    setState(
+      () {
+        filteredNotes = notes
+            .where(
+                (item) => item.path.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      },
+    );
+  }
 
   @override
   void initState() {
@@ -29,6 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
       widget.storage.readNote(value[selectedNote]).then((note) {
         setState(() {
           notes = value;
+          filteredNotes = value;
           root = OrgDocument.parse(note);
         });
       });
@@ -40,31 +59,58 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         drawer: Drawer(
-          child: ListView(padding: EdgeInsets.zero, children: [
-            const DrawerHeader(
-              child: Text('Notes'),
-            ),
-            for (final (index, note) in notes.indexed)
-              ListTile(
-                dense: true,
-                contentPadding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
-                selected: index == selectedNote,
-                minTileHeight: 4.0,
-                selectedColor: Theme.of(context).colorScheme.primary,
-                selectedTileColor: Theme.of(context).focusColor,
-                title: Text(note.path.split('/').last),
-                onTap: () {
-                  widget.storage.readNote(note).then((value) {
-                    setState(() {
-                      selectedNote = index;
-                      root = OrgDocument.parse(value);
-                    });
-                    Navigator.pop(context);
-                  });
-                },
-              )
-          ]),
+          child: ListView(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 0),
+              children: [
+                DrawerHeader(
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (value) {
+                          search(value);
+                        },
+                        onSubmitted: (_) {
+                          widget.storage
+                              .readNote(filteredNotes[0])
+                              .then((value) {
+                            setState(() {
+                              root = OrgDocument.parse(value);
+                            });
+                            Navigator.pop(context);
+                          });
+                        },
+                        decoration: const InputDecoration(
+                          hintText: 'Search...',
+                          prefixIcon: Icon(
+                            Icons.search,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                for (final (index, note) in filteredNotes.indexed)
+                  ListTile(
+                    dense: true,
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 8.0, vertical: 0.0),
+                    selected: index == selectedNote,
+                    minTileHeight: 4.0,
+                    selectedColor: Theme.of(context).colorScheme.primary,
+                    selectedTileColor: Theme.of(context).focusColor,
+                    title: Text(note.path.split('/').last),
+                    onTap: () {
+                      widget.storage.readNote(note).then((value) {
+                        setState(() {
+                          selectedNote = index;
+                          root = OrgDocument.parse(value);
+                        });
+                        Navigator.pop(context);
+                      });
+                    },
+                  )
+              ]),
         ),
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
