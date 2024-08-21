6fb05b8fcda2ed35c82fadf9114c4514bb6e409b
linchen2chris <lc1990linux@gmail.com>
2024-08-21 16:16:28 +0800

    close drawer when select(TPDI4-111)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -48,6 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     setState(() {
                       root = OrgDocument.parse(value);
                     });
+                    Navigator.pop(context);
                   });
                 },
               )
